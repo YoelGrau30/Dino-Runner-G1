8e5e4d07124769c43ecc09d3b1eabdd44069eb52
YoelGrau30 <y.yoelgrau07@gmail.com>
2023-03-31 14:40:21 -0500

    se le baja la velocidad de cambio de imagen

--- dino_runner/components/Mario.py
+++ dino_runner/components/Mario.py
@@ -15,13 +15,16 @@ class Mario(Sprite):
 
     def update(self):
         self.run()
+
         self.step += 1
+        if self.step == 10:
+            self.step = 0
 
     def draw(self, screen):
         screen.blit(self.image, (self.mario_rect.x, self.mario_rect.y))
 
     def run(self):
-        if self.step % 2 == 0:
+        if self.step < 5:
             self.image = RUNNING[0]
         else:
             self.image = RUNNING[1]
