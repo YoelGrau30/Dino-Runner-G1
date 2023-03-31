9b6d334ad3f824dc9fb77c8f8519d531796e0d29
YoelGrau30 <y.yoelgrau07@gmail.com>
2023-03-30 22:57:31 -0500

    cambio la velocidad

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -11,7 +11,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
-        self.game_speed = 20
+        self.game_speed = 16
         self.x_pos_bg = 0
         self.y_pos_bg = 442
 
