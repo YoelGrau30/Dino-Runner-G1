4c9b4c504b6a02b4e5fbacce7c8b5cd66c7efe59
YoelGrau30 <y.yoelgrau07@gmail.com>
2023-04-03 18:06:28 -0500

    ADD

--- dino_runner/utils/constants_enemys.py
+++ dino_runner/utils/constants_enemys.py
@@ -5,158 +5,158 @@ IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
 #-------------------------------------------------------------------------------------------------------
 # ESTRUCTURA OBSTACULO TUNEL LARGO
 #-------------------------------------------------------------------------------------------------------
-LARGE_TUNNEL_ANCHO_1 = 48
-LARGE_TUNNEL_ALTO_1 = 95
+LARGE_TUNNEL_WIDTH_1 = 48
+LARGE_TUNNEL_HEIGHT_1 = 95
 
-LARGE_TUNNEL_ANCHO_2 = 99
-LARGE_TUNNEL_ALTO_2 = 95
+LARGE_TUNNEL_WIDTH_2 = 99
+LARGE_TUNNEL_HEIGHT_2 = 95
 
-LARGE_TUNNEL_ANCHO_3 = 102
-LARGE_TUNNEL_ALTO_3 = 95
+LARGE_TUNNEL_WIDTH_3 = 102
+LARGE_TUNNEL_HEIGHT_3 = 95
 
 
 LARGE_TUNNEL = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/LargeTunnel1.png")), (LARGE_TUNNEL_ANCHO_1, LARGE_TUNNEL_ALTO_1)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/LargeTunnel2.png")), (LARGE_TUNNEL_ANCHO_2, LARGE_TUNNEL_ALTO_2)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/LargeTunnel3.png")), (LARGE_TUNNEL_ANCHO_3, LARGE_TUNNEL_ALTO_3)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/LargeTunnel1.png")), (LARGE_TUNNEL_WIDTH_1, LARGE_TUNNEL_HEIGHT_1)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/LargeTunnel2.png")), (LARGE_TUNNEL_WIDTH_2, LARGE_TUNNEL_HEIGHT_2)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/LargeTunnel3.png")), (LARGE_TUNNEL_WIDTH_3, LARGE_TUNNEL_HEIGHT_3)),
 ]
 #-------------------------------------------------------------------------------------------------------
 # ESTRUCTURA OBSTACULO TUNEL PEQUEÑO
 #-------------------------------------------------------------------------------------------------------
-SMALL_TUNNEL_ANCHO_1 = 40
-SMALL_TUNNEL_ALTO_1 = 71
+SMALL_TUNNEL_WIDTH_1 = 40
+SMALL_TUNNEL_HEIGHT_1 = 71
 
-SMALL_TUNNEL_ANCHO_2 = 68
-SMALL_TUNNEL_ALTO_2 = 71
+SMALL_TUNNEL_WIDTH_2 = 68
+SMALL_TUNNEL_HEIGHT_2 = 71
 
-SMALL_TUNNEL_ANCHO_3 = 105
-SMALL_TUNNEL_ALTO_3 = 71
+SMALL_TUNNEL_WIDTH_3 = 105
+SMALL_TUNNEL_HEIGHT_3 = 71
 
 SMALL_TUNNEL = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/SmallTunnel1.png")), (SMALL_TUNNEL_ANCHO_1, SMALL_TUNNEL_ALTO_1)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/SmallTunnel2.png")), (SMALL_TUNNEL_ANCHO_2, SMALL_TUNNEL_ALTO_2)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/SmallTunnel3.png")), (SMALL_TUNNEL_ANCHO_3, SMALL_TUNNEL_ALTO_3)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/SmallTunnel1.png")), (SMALL_TUNNEL_WIDTH_1, SMALL_TUNNEL_HEIGHT_1)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/SmallTunnel2.png")), (SMALL_TUNNEL_WIDTH_2, SMALL_TUNNEL_HEIGHT_2)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Tunnel/SmallTunnel3.png")), (SMALL_TUNNEL_WIDTH_3, SMALL_TUNNEL_HEIGHT_3)),
 ]
 
 #-------------------------------------------------------------------------------------------------------
 # ESTRUCTURA DE ENEMIGOS
 #-------------------------------------------------------------------------------------------------------
-BILL_BALA_ANCHO = 60
-BILL_BALA_ALTO = 70
+BILL_BALA_WIDTH = 60
+BILL_BALA_HEIGHT = 70
 BILL_BALA = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/BillBala1.png")), (BILL_BALA_ANCHO, BILL_BALA_ALTO)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/BillBala2.png")), (BILL_BALA_ANCHO, BILL_BALA_ALTO)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/BillBala1.png")), (BILL_BALA_WIDTH, BILL_BALA_HEIGHT)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/BillBala2.png")), (BILL_BALA_WIDTH, BILL_BALA_HEIGHT)),
 ]
 
 ########################################################################################################################################
 
-BOWSER_ANCHO = 60
-BOWSER_ALTO = 70
+BOWSER_WIDTH = 60
+BOWSER_HEIGHT = 70
 BOWSER = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Bowser1.png")), (BOWSER_ANCHO, BOWSER_ALTO)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Bowser2.png")), (BOWSER_ANCHO, BOWSER_ALTO)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Bowser1.png")), (BOWSER_WIDTH, BOWSER_HEIGHT)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Bowser2.png")), (BOWSER_WIDTH, BOWSER_HEIGHT)),
 ]
 
 ########################################################################################################################################
 
-BUM_ANCHO = 60
-BUM_ALTO = 70
+BUM_WIDTH = 60
+BUM_HEIGHT = 70
 BUM = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Bum1.png")), (BUM_ANCHO, BUM_ALTO)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Bum2.png")), (BUM_ANCHO, BUM_ALTO)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Bum1.png")), (BUM_WIDTH, BUM_HEIGHT)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Bum2.png")), (BUM_WIDTH, BUM_HEIGHT)),
 ]
 
 ########################################################################################################################################
 
-CHAIN_CHOMP_ANCHO = 60
-CHAIN_CHOMP_ALTO = 70
+CHAIN_CHOMP_WIDTH = 60
+CHAIN_CHOMP_HEIGHT = 70
 CHAIN_CHOMP = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/ChainChomp1.png")), (CHAIN_CHOMP_ANCHO, CHAIN_CHOMP_ALTO)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/ChainChomp2.png")), (CHAIN_CHOMP_ANCHO, CHAIN_CHOMP_ALTO)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/ChainChomp1.png")), (CHAIN_CHOMP_WIDTH, CHAIN_CHOMP_HEIGHT)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/ChainChomp2.png")), (CHAIN_CHOMP_WIDTH, CHAIN_CHOMP_HEIGHT)),
 ]
 
 ########################################################################################################################################
 
-DRY_BOWSER_ANCHO = 60
-DRY_BOWSER_ALTO = 70
+DRY_BOWSER_WIDTH = 60
+DRY_BOWSER_HEIGHT = 70
 DRY_BOWSER = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/DryBowser1.png")), (DRY_BOWSER_ANCHO, DRY_BOWSER_ALTO)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/DryBowser2.png")), (DRY_BOWSER_ANCHO, DRY_BOWSER_ALTO)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/DryBowser1.png")), (DRY_BOWSER_WIDTH, DRY_BOWSER_HEIGHT)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/DryBowser2.png")), (DRY_BOWSER_WIDTH, DRY_BOWSER_HEIGHT)),
 ]
 
 ########################################################################################################################################
 
-GHOST_ANCHO = 60
-GHOST_ALTO = 70
+GHOST_WIDTH = 60
+GHOST_HEIGHT = 70
 GHOST = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Ghost1.png")), (GHOST_ANCHO, GHOST_ALTO)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Ghost2.png")), (GHOST_ANCHO, GHOST_ALTO)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Ghost1.png")), (GHOST_WIDTH, GHOST_HEIGHT)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Ghost2.png")), (GHOST_WIDTH, GHOST_HEIGHT)),
 ]
 
 ########################################################################################################################################
 
-THWOMP_ANCHO = 60
-THWOMP_ALTO = 70
-THWOMP = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Thwomp1.png")), (THWOMP_ANCHO, THWOMP_ALTO)),
+THWOMP_WIDTH = 60
+THWOMP_HEIGHT = 70
+THWOMP = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/Thwomp1.png")), (THWOMP_WIDTH, THWOMP_HEIGHT)),
 
 ########################################################################################################################################
 
-TROPPA_RED_ANCHO = 60
-TROPPA_RED_ALTO = 70
+TROPPA_RED_WIDTH = 60
+TROPPA_RED_HEIGHT = 70
 TROPPA_RED = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TroppaRed1.png")), (TROPPA_RED_ANCHO, TROPPA_RED_ALTO)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TroppaRed2.png")), (TROPPA_RED_ANCHO, TROPPA_RED_ALTO)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TroppaRed1.png")), (TROPPA_RED_WIDTH, TROPPA_RED_HEIGHT)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TroppaRed2.png")), (TROPPA_RED_WIDTH, TROPPA_RED_HEIGHT)),
 ]
 
 ########################################################################################################################################
 
-TURTLE_ANCHO = 60
-TURTLE_ALTO = 70
+TURTLE_WIDTH = 60
+TURTLE_HEIGHT = 70
 
 TURTLE = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleBlue.png")), (TURTLE_ANCHO, TURTLE_ALTO)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleRed.png")), (TURTLE_ANCHO, TURTLE_ALTO)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleGreen.png")), (TURTLE_ANCHO, TURTLE_ALTO)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleBlue.png")), (TURTLE_WIDTH, TURTLE_HEIGHT)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleRed.png")), (TURTLE_WIDTH, TURTLE_HEIGHT)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleGreen.png")), (TURTLE_WIDTH, TURTLE_HEIGHT)),
 ]
 
 ########################################################################################################################################
 
-TURTLE_FLY_BLUE_ANCHO_1 = 60
-TURTLE_FLY_BLUE_ALTO_1 = 70
+TURTLE_FLY_BLUE_WIDTH_1 = 60
+TURTLE_FLY_BLUE_HEIGHT_1 = 70
 
-TURTLE_FLY_BLUE_ANCHO_2 = 60
-TURTLE_FLY_BLUE_ALTO_2 = 70
+TURTLE_FLY_BLUE_WIDTH_2 = 60
+TURTLE_FLY_BLUE_HEIGHT_2 = 70
 
 TURTLE_FLY_BLUE = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyBlue1.png")), (TURTLE_FLY_BLUE_ANCHO_1, TURTLE_FLY_BLUE_ALTO_1)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyBlue2.png")), (TURTLE_FLY_BLUE_ANCHO_2, TURTLE_FLY_BLUE_ALTO_2)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyBlue1.png")), (TURTLE_FLY_BLUE_WIDTH_1, TURTLE_FLY_BLUE_HEIGHT_1)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyBlue2.png")), (TURTLE_FLY_BLUE_WIDTH_2, TURTLE_FLY_BLUE_HEIGHT_2)),
 ]
 
 ########################################################################################################################################
 
-TURTLE_FLY_RED_ANCHO_1 = 60
-TURTLE_FLY_RED_ALTO_1 = 70
+TURTLE_FLY_RED_WIDTH_1 = 60
+TURTLE_FLY_RED_HEIGHT_1 = 70
 
-TURTLE_FLY_RED_ANCHO_2 = 60
-TURTLE_FLY_RED_ALTO_2 = 70
+TURTLE_FLY_RED_WIDTH_2 = 60
+TURTLE_FLY_RED_HEIGHT_2 = 70
 
 
 TURTLE_FLY_RED = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyRed1.png")), (TURTLE_FLY_RED_ANCHO_1, TURTLE_FLY_RED_ALTO_1)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyRed2.png")), (TURTLE_FLY_RED_ANCHO_2, TURTLE_FLY_RED_ALTO_2)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyRed1.png")), (TURTLE_FLY_RED_WIDTH_1, TURTLE_FLY_RED_HEIGHT_1)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyRed2.png")), (TURTLE_FLY_RED_WIDTH_2, TURTLE_FLY_RED_HEIGHT_2)),
 ]
 
 ########################################################################################################################################
 
-TURTLE_FLY_GREEN_ANCHO_1 = 60
-TURTLE_FLY_GREEN_ALTO_1 = 70
+TURTLE_FLY_GREEN_WIDTH_1 = 60
+TURTLE_FLY_GREEN_HEIGHT_1 = 70
 
-TURTLE_FLY_GREEN_ANCHO_2 = 60
-TURTLE_FLY_GREEN_ALTO_2 = 70
+TURTLE_FLY_GREEN_WIDTH_2 = 60
+TURTLE_FLY_GREEN_HEIGHT_2 = 70
 
 TURTLE_FLY_GREEN = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyGreen1.png")), (TURTLE_FLY_GREEN_ANCHO_1, TURTLE_FLY_GREEN_ALTO_1)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyGreen2.png")), (TURTLE_FLY_GREEN_ANCHO_2, TURTLE_FLY_GREEN_ALTO_2)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyGreen1.png")), (TURTLE_FLY_GREEN_WIDTH_1, TURTLE_FLY_GREEN_HEIGHT_1)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Enemys/TurtleFlyGreen2.png")), (TURTLE_FLY_GREEN_WIDTH_2, TURTLE_FLY_GREEN_HEIGHT_2)),
 ]
 
 ########################################################################################################################################
--- dino_runner/utils/constants_other_mario.py
+++ dino_runner/utils/constants_other_mario.py
@@ -26,145 +26,145 @@ FOSIL = pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Fosil.png"))
 
 ########################################################################################################################################
 
-PRINCESS_ANCHO = 70
-PRINCESS_ALTO = 70
+PRINCESS_WIDTH = 70
+PRINCESS_HEIGHT = 70
 
 PRINCESS = [
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Princess1.png")), (PRINCESS_ANCHO, PRINCESS_ALTO)),
-    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Princess2.png")), (PRINCESS_ANCHO, PRINCESS_ALTO)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Princess1.png")), (PRINCESS_WIDTH, PRINCESS_HEIGHT)),
+    pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Princess2.png")), (PRINCESS_WIDTH, PRINCESS_HEIGHT)),
     ]
 
 ########################################################################################################################################
 
-CASTLE_ANCHO = 215
-CASTLE_ALTO = 355
+CASTLE_WIDTH = 215
+CASTLE_HEIGHT = 355
 
-CASTLE = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Castle.png")), (CASTLE_ANCHO, CASTLE_ALTO))
-
-########################################################################################################################################
-
-CONGRAT_ANCHO = 300
-CONGRAT_ALTO = 150
-
-CONGRATULATIONS = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Congratulations.png")), (CONGRAT_ANCHO, CONGRAT_ALTO))
+CASTLE = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Castle.png")), (CASTLE_WIDTH, CASTLE_HEIGHT))
 
 ########################################################################################################################################
 
-DEAD_COUNT_ANCHO = 40
-DEAD_COUNT_ALTO = 40
+CONGRAT_WIDTH = 300
+CONGRAT_HEIGHT = 150
 
-DEAD_COUNT = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/DeadCount.png")), (DEAD_COUNT_ANCHO, DEAD_COUNT_ALTO))
-
-########################################################################################################################################
-
-DEREC_ANCHO = 150
-DEREC_ALTO = 200
-
-DEREC = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Derec.png")), (DEREC_ANCHO, DEREC_ALTO))
+CONGRATULATIONS = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Congratulations.png")), (CONGRAT_WIDTH, CONGRAT_HEIGHT))
 
 ########################################################################################################################################
 
-EXIT_ANCHO = 60
-EXIT_ALTO = 60
+DEAD_COUNT_WIDTH = 40
+DEAD_COUNT_HEIGHT = 40
 
-EXIT = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Exit.png")), (EXIT_ANCHO, EXIT_ALTO))
-
-########################################################################################################################################
-
-EXPLOTION_ANCHO = 120
-EXPLOTION_ALTO = 120
-
-EXPLOTION = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Explotion.png")), (DEREC_ANCHO, DEREC_ALTO))
+DEAD_COUNT = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/DeadCount.png")), (DEAD_COUNT_WIDTH, DEAD_COUNT_HEIGHT))
 
 ########################################################################################################################################
 
-OVER_ANCHO = 60
-OVER_ALTO = 60
+DEREC_WIDTH = 150
+DEREC_HEIGHT = 200
 
-GAME_OVER = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/GameOver.png")), (OVER_ANCHO, OVER_ALTO))
-
-########################################################################################################################################
-
-GIGANT_ANCHO = 50
-GIGANT_ALTO = 50
-
-GIGANT = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Gigant.png")), (GIGANT_ANCHO, GIGANT_ALTO))
+DEREC = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Derec.png")), (DEREC_WIDTH, DEREC_HEIGHT))
 
 ########################################################################################################################################
 
-HEART_ANCHO = 50
-HEART_ALTO = 50
+EXIT_WIDTH = 60
+EXIT_HEIGHT = 60
 
-HEART = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Heart.png")), (HEART_ANCHO, HEART_ALTO))
-
-########################################################################################################################################
-
-IZQ_ANCHO = 150
-IZQ_ALTO = 200
-
-IZQ = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Izq.png")), (IZQ_ANCHO, IZQ_ALTO))
+EXIT = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Exit.png")), (EXIT_WIDTH, EXIT_HEIGHT))
 
 ########################################################################################################################################
 
-KEY_CASTLE_ANCHO = 50
-KEY_CASTLE_ALTO = 50
+EXPLOTION_WIDTH = 120
+EXPLOTION_HEIGHT = 120
 
-KEY_CASTLE = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/KeyCastle.png")), (KEY_CASTLE_ANCHO, KEY_CASTLE_ALTO))
-
-########################################################################################################################################
-
-LOGO_POINTS_ANCHO = 40
-LOGO_POINTS_ALTO = 40
-
-LOGO_POINTS = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/LogoPoints.png")), (LOGO_POINTS_ANCHO, LOGO_POINTS_ALTO))
+EXPLOTION = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Explotion.png")), (DEREC_WIDTH, DEREC_HEIGHT))
 
 ########################################################################################################################################
 
-MARIO_DEAD_ANCHO = 300
-MARIO_DEAD_ALTO = 64
+OVER_WIDTH = 60
+OVER_HEIGHT = 60
 
-MARIO_DEAD = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/MarioDead.png")), (MARIO_DEAD_ANCHO, MARIO_DEAD_ALTO))
-
-########################################################################################################################################
-
-MARIO_LOVE_ANCHO = 300
-MARIO_LOVE_ALTO = 300
-
-MARIO_LOVE = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/MarioLove.png")), (MARIO_LOVE_ANCHO, MARIO_LOVE_ALTO))
+GAME_OVER = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/GameOver.png")), (OVER_WIDTH, OVER_HEIGHT))
 
 ########################################################################################################################################
 
-PLAY_ANCHO = 40
-PLAY_ALTO = 40
+GIGANT_WIDTH = 50
+GIGANT_HEIGHT = 50
 
-PLAY = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Play.png")), (PLAY_ANCHO, PLAY_ALTO))
-
-########################################################################################################################################
-
-SHIELD_ANCHO = 50
-SHIELD_ALTO = 50
-SHIELD = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Shield.png")), (SHIELD_ANCHO, SHIELD_ALTO))
+GIGANT = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Gigant.png")), (GIGANT_WIDTH, GIGANT_HEIGHT))
 
 ########################################################################################################################################
 
-SUPER_MARIO_ANCHO = 50
-SUPER_MARIO_ALTO = 50
+HEART_WIDTH = 50
+HEART_HEIGHT = 50
 
-SUPER_MARIO = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/SuperMario.png")), (SUPER_MARIO_ANCHO, SUPER_MARIO_ALTO))
-
-########################################################################################################################################
-
-SURPRISE_ANCHO = 50
-SURPRISE_ALTO = 50
-
-SURPRISE = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Surprise.png")), (SURPRISE_ANCHO, SURPRISE_ALTO))
+HEART = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Heart.png")), (HEART_WIDTH, HEART_HEIGHT))
 
 ########################################################################################################################################
 
-YUSHI_ANCHO = 50
-YUSHI_ALTO = 50
+IZQ_WIDTH = 150
+IZQ_HEIGHT = 200
 
-YUSHI = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Yushi.png")), (YUSHI_ANCHO, YUSHI_ALTO))
+IZQ = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Izq.png")), (IZQ_WIDTH, IZQ_HEIGHT))
+
+########################################################################################################################################
+
+KEY_CASTLE_WIDTH = 50
+KEY_CASTLE_HEIGHT = 50
+
+KEY_CASTLE = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/KeyCastle.png")), (KEY_CASTLE_WIDTH, KEY_CASTLE_HEIGHT))
+
+########################################################################################################################################
+
+LOGO_POINTS_WIDTH = 40
+LOGO_POINTS_HEIGHT = 40
+
+LOGO_POINTS = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/LogoPoints.png")), (LOGO_POINTS_WIDTH, LOGO_POINTS_HEIGHT))
+
+########################################################################################################################################
+
+MARIO_DEAD_WIDTH = 300
+MARIO_DEAD_HEIGHT = 64
+
+MARIO_DEAD = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/MarioDead.png")), (MARIO_DEAD_WIDTH, MARIO_DEAD_HEIGHT))
+
+########################################################################################################################################
+
+MARIO_LOVE_WIDTH = 300
+MARIO_LOVE_HEIGHT = 300
+
+MARIO_LOVE = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/MarioLove.png")), (MARIO_LOVE_WIDTH, MARIO_LOVE_HEIGHT))
+
+########################################################################################################################################
+
+PLAY_WIDTH = 40
+PLAY_HEIGHT = 40
+
+PLAY = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Play.png")), (PLAY_WIDTH, PLAY_HEIGHT))
+
+########################################################################################################################################
+
+SHIELD_WIDTH = 50
+SHIELD_HEIGHT = 50
+SHIELD = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Shield.png")), (SHIELD_WIDTH, SHIELD_HEIGHT))
+
+########################################################################################################################################
+
+SUPER_MARIO_WIDTH = 50
+SUPER_MARIO_HEIGHT = 50
+
+SUPER_MARIO = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/SuperMario.png")), (SUPER_MARIO_WIDTH, SUPER_MARIO_HEIGHT))
+
+########################################################################################################################################
+
+SURPRISE_WIDTH = 50
+SURPRISE_HEIGHT = 50
+
+SURPRISE = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Surprise.png")), (SURPRISE_WIDTH, SURPRISE_HEIGHT))
+
+########################################################################################################################################
+
+YUSHI_WIDTH = 50
+YUSHI_HEIGHT = 50
+
+YUSHI = pygame.transform.scale(pygame.image.load(os.path.join(IMG_DIR, "Mario_Other/Yushi.png")), (YUSHI_WIDTH, YUSHI_HEIGHT))
 
 ########################################################################################################################################
 
