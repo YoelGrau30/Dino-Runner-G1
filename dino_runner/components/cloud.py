be85721841f70a0703bcdeee7f4b02eafa07511d
YoelGrau30 <y.yoelgrau07@gmail.com>
2023-04-04 21:10:40 -0500

    cloud sen

--- dino_runner/components/cloud.py
+++ dino_runner/components/cloud.py
@@ -1,12 +1,15 @@
 import random
 import pygame
+import math
 from pygame.sprite import Sprite
 from dino_runner.utils.constants_other_mario import CLOUD
 
 class CloudMario(Sprite):
 
     X_POS = 1000
-    Y_POS = random.randint(100, 400)
+    Y_POS = 250 
+    AMPLITUDE = 145 # Altura de la onda
+    PERIOD = 90 #70     # Cantidad de frames por ciclo de onda
 
     def __init__(self):
         self.image = CLOUD[0]
@@ -16,15 +19,14 @@ class CloudMario(Sprite):
         self.step = 0
 
     def update(self):
-
-        self.rect.x -= 5 
-        self.image = CLOUD[self.step // 5]
+        self.rect.x -= 5
         self.step += 1
-        if self.step >= 10:
-            self.step = 0
+        y_offset = self.AMPLITUDE * \
+                   math.sin(2 * math.pi * self.step / self.PERIOD)
+        self.rect.centery = self.Y_POS + y_offset
+        self.image = CLOUD[self.step // 5 % len(CLOUD)] # hace mas lenta la sucesion de la nube
         if self.rect.right < 0:
-            self.rect.left = 1000 
-            self.rect.centery = random.randint(100, 300) 
+            self.rect.left = 1000
 
     def draw(self, screen: pygame.Surface):
         screen.blit(self.image, (self.rect.x, self.rect.y))
